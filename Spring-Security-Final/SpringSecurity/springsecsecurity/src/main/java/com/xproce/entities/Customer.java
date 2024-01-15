61a158bd1cf84b8a273fcaaa6af73f9777aab5fe
Kali-div <83927280+Kali-div@users.noreply.github.com>
2024-01-15 15:58:44 +0100

    Add files via upload

---
+++ Spring-Security-Final/SpringSecurity/springsecsecurity/src/main/java/com/xproce/entities/Customer.java
@@ -0,0 +1,26 @@
+package com.xproce.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.*;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@ToString
+@Builder
+public class Customer {
+
+    @Id @GeneratedValue (strategy = GenerationType.IDENTITY)
+    private Long Id;
+    private String email;
+    private String password;
+    private String role;
+
+    public String getPwd() {
+        return this.password;
+    }
+}
